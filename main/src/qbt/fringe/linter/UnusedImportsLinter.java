5f7307ef590aa99ad1dce86cb1fcb6914e5582f4
Keith Amling <keith.amling@gmail.com>
2015-10-11 16:16:11 -0700

    Check unused imports.

---
+++ main/src/qbt/fringe/linter/UnusedImportsLinter.java
@@ -0,0 +1,123 @@
+package qbt.fringe.linter;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Multimap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class UnusedImportsLinter implements Linter {
+    enum State {
+        ZERO,
+        SLASH,
+        LINE_COMMENT,
+        BIG_COMMENT,
+        BIG_COMMENT_STAR,
+        ;
+    }
+
+    private static final Pattern IMPORT_PATTERN = Pattern.compile("import [^ ]*\\.([^ \\.]*);\n");
+
+    @Override
+    public List<Pair<Integer, String>> check(List<String> lines) {
+        final Multimap<String, Integer> unusedImports = HashMultimap.create();
+        for(int i = 0; i < lines.size(); ++i) {
+            Matcher m = IMPORT_PATTERN.matcher(lines.get(i));
+            if(m.matches()) {
+                unusedImports.put(m.group(1), i);
+            }
+        }
+
+        State s = State.ZERO;
+        class Id {
+            StringBuffer sb = new StringBuffer();
+
+            public void append(char c) {
+                if(c == '_' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+                    sb.append(c);
+                }
+                else {
+                    sep();
+                }
+            }
+
+            public void sep() {
+                int len = sb.length();
+                if(len > 0) {
+                    unusedImports.asMap().remove(sb.toString());
+                    sb.delete(0, len);
+                }
+            }
+        }
+        Id id = new Id();
+        for(int i = 0; i < lines.size(); ++i) {
+            String line = lines.get(i);
+            for(int j = 0; j < line.length(); ++j) {
+                char c = line.charAt(j);
+                switch(s) {
+                    case ZERO:
+                        if(c == '/') {
+                            s = State.SLASH;
+                        }
+                        else {
+                            id.append(c);
+                        }
+                        break;
+
+                    case SLASH:
+                        if(c == '/') {
+                            id.sep();
+                            s = State.LINE_COMMENT;
+                        }
+                        else if(c == '*') {
+                            id.sep();
+                            s = State.BIG_COMMENT;
+                        }
+                        else {
+                            id.append('/');
+                            id.append(c);
+                            s = State.ZERO;
+                        }
+                        break;
+
+                    case LINE_COMMENT:
+                        if(c == '\n') {
+                            s = State.ZERO;
+                        }
+                        break;
+
+                    case BIG_COMMENT:
+                        if(c == '*') {
+                            s = State.BIG_COMMENT_STAR;
+                        }
+                        break;
+
+                    case BIG_COMMENT_STAR:
+                        if(c == '/') {
+                            s = State.ZERO;
+                        }
+                        if(c == '*') {
+                            // nothing, remain in BIG_COMMENT_STAR
+                        }
+                        else {
+                            s = State.BIG_COMMENT;
+                        }
+                        break;
+
+                    default:
+                        throw new IllegalStateException(String.valueOf(s));
+                }
+            }
+        }
+        id.sep();
+
+        ImmutableList.Builder<Pair<Integer, String>> b = ImmutableList.builder();
+        for(Map.Entry<String, Integer> e : unusedImports.entries()) {
+            b.add(Pair.of(e.getValue(), "Unused import of " + e.getKey()));
+        }
+        return b.build();
+    }
+}
